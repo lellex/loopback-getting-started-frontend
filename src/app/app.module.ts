732e27ab8de5bcb66d5088beea0ed3044f851605
Lellex <contact.ajanin@gmail.com>
2017-06-26 21:45:58 +0200

    Add routes and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule }    from '@angular/http';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CoffeeshopService } from './coffeeshops/coffeeshop.service';
@@ -8,16 +9,41 @@ import { ReviewsService } from './reviews/reviews.service';
 
 import { CoffeeshopsComponent } from './coffeeshops/coffeeshops.component';
 import { ReviewsComponent } from './reviews/reviews.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AddReviewComponent } from './add-review/add-review.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoffeeshopsComponent,
-    ReviewsComponent
+    ReviewsComponent,
+    LoginComponent,
+    SignupComponent,
+    AddReviewComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/coffeeshops',
+        pathMatch: 'full'
+      },
+      {
+        path: 'coffeeshops',
+        component: CoffeeshopsComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'signup',
+        component: SignupComponent
+      }
+    ])
   ],
   providers: [
     CoffeeshopService,
